2d1e844dda7f66234ba801a61fa53124c8b8bced
Garbonauta <eduardo.moreno.j@gmail.com>
2018-09-09 18:35:54 -0500

    Initial Commit

---
+++ gatsby-node.js
@@ -0,0 +1,14 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// eslint-disable-next-line no-undef
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [ 'src' ],
+    },
+  });
+};
