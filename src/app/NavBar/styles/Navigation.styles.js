5d530aa05d7703df1a1fb7d53a2d69a23d953f0b
Garbonauta <eduardo.moreno.j@gmail.com>
2019-01-03 23:11:13 -0600

    tweaked navigation link

--- src/app/NavBar/styles/Navigation.styles.js
+++ src/app/NavBar/styles/Navigation.styles.js
@@ -20,7 +20,7 @@ export const NavigationLink = styled(Link)`
   text-transform: uppercase;
   font-size: 1.05rem;
   font-weight: 600;
-  letter-spacing: 3px;
+  letter-spacing: 2px;
 
   &:hover {
     color: #ffffff;
