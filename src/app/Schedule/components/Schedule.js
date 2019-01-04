301232396157a4bbca60b44d00b09be6927e7a9e
Garbonauta <eduardo.moreno.j@gmail.com>
2019-01-03 22:28:03 -0600

    Created schedule page.

---
+++ src/app/Schedule/components/Schedule.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+
+import { Banner } from 'app/Banner';
+
+import floorImage from 'images/floor.jpg';
+
+const Schedule = () => (
+  <Fragment>
+    <Helmet>
+      <title>Schedule</title>
+    </Helmet>
+    <Banner
+      image={{
+        source: floorImage,
+        alt: 'Students Drilling reps on the ground',
+      }}
+    />
+  </Fragment>
+);
+
+export default Schedule;
