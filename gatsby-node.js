708f842064cf2d9f03ea91e04d0764242558295a
Garbonauta <eduardo.moreno.j@gmail.com>
2019-01-27 20:27:06 -0600

    fixed issue as per documentation

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,12 +3,13 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require('path');
 
 // eslint-disable-next-line no-undef
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
-      modules: ['node_modules', 'src'],
+      modules: ['node_modules', path.resolve(__dirname, 'src')],
     },
   });
 };
