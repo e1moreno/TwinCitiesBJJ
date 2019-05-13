a3bec10e7a711fb5b9d1e0d8a796a2f4119f2573
Garbonauta <eduardo.moreno.j@gmail.com>
2019-05-12 23:43:18 -0500

    Contact page restyling.

--- src/app/Join/styles/Join.styles.js
+++ src/app/Join/styles/Join.styles.js
@@ -13,7 +13,7 @@ export const PageSection = styled(BasePageSection)`
     height: 108%;
     top: 0;
     z-index: -1;
-    transform: skewY(-8deg);
+    transform: skewY(-6deg);
     background-color: #ffffff;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cg fill='%23fbfbfb' fill-opacity='0.89'%3E%3Cpath fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/%3E%3C/g%3E%3C/svg%3E");
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2), 0px -1px 8px rgba(0, 0, 0, 0.15);
