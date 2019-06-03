50938919c654b28d516ae0eb962e533503d25673
Garbonauta <eduardo.moreno.j@gmail.com>
2019-06-02 21:25:19 -0500

    Phone Form and Validation.

---
+++ src/app/JoinForm/components/PhoneInput.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { InputWrapper, Label, Input } from '../styles/PhoneInput.styles';
+
+const PhoneInput = ({
+  focus,
+  labelText,
+  labelProps,
+  id,
+  name,
+  value,
+  onChange,
+  onFocus,
+  onBlur,
+  ...other
+}) => (
+  <InputWrapper>
+    <Label htmlFor={id} {...labelProps} focus={focus}>
+      {!!labelText && labelText}
+      <Input
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        mask={[
+          '(',
+          /[1-9]/,
+          /\d/,
+          /\d/,
+          ')',
+          ' ',
+          /\d/,
+          /\d/,
+          /\d/,
+          '-',
+          /\d/,
+          /\d/,
+          /\d/,
+          /\d/,
+        ]}
+        guide={false}
+        {...other}
+      />
+    </Label>
+  </InputWrapper>
+);
+
+PhoneInput.propTypes = {
+  focus: PropTypes.bool,
+  labelText: PropTypes.string,
+  labelProps: PropTypes.object,
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
+  onFocus: PropTypes.func.isRequired,
+};
+PhoneInput.defaultProps = {
+  focus: false,
+  labelText: null,
+  labelProps: {},
+};
+
+export default PhoneInput;
