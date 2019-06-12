76c6c0911b4c8a2aa03887539b1bdd48382b312d
Eduardo Moreno <eduardo.moreno.j@gmail.com>
2019-06-12 01:11:49 +0000

    Upgraded react image

--- src/app/ImageGallery/containers/ImageGalleryContainer.js
+++ src/app/ImageGallery/containers/ImageGalleryContainer.js
@@ -1,6 +1,6 @@
 import React, { useContext, useCallback } from 'react';
 
-import Lightbox from 'react-images';
+import Carousel, { Modal, ModalGateway } from 'react-images';
 import { ImageGalleryContext } from '../context/ImageGalleryContextProvider';
 
 const ImageGalleryContainer = () => {
@@ -8,36 +8,18 @@ const ImageGalleryContainer = () => {
     open, currentImage, src, dispatch,
   } = useContext(ImageGalleryContext);
 
-  const handlePrevious = useCallback(() => {
-    if (currentImage > 0) {
-      dispatch({ type: 'setCurrentImage', currentImage: currentImage - 1 });
-    }
-  }, [currentImage]);
-
-  const handleNext = useCallback(() => {
-    const nextImage = currentImage + 1;
-
-    if (nextImage < src.length) {
-      dispatch({ type: 'setCurrentImage', currentImage: nextImage });
-    }
-  }, [src, currentImage]);
-
   const handleClose = useCallback(() => {
     dispatch({ type: 'setOpen', open: false });
   }, [dispatch]);
 
   return (
-    <Lightbox
-      images={src}
-      isOpen={open}
-      currentImage={currentImage}
-      onClickPrev={handlePrevious}
-      onClickNext={handleNext}
-      onClickImage={handleNext}
-      onClose={handleClose}
-      showImageCount={false}
-      backdropClosesModal
-    />
+    <ModalGateway>
+      {open && (
+        <Modal onClose={handleClose}>
+          <Carousel views={src} currentIndex={currentImage} />
+        </Modal>
+      )}
+    </ModalGateway>
   );
 };
 
