7745dff02006ae4ea503f76c403c0f76640a75a9
Garbonauta <eduardo.moreno.j@gmail.com>
2019-11-07 23:00:52 -0600

    console

--- src/layout/containers/useFeatureToggle.js
+++ src/layout/containers/useFeatureToggle.js
@@ -5,7 +5,6 @@ const useFeatureToggle = (featureSymbol) => {
   const feature = Symbol.keyFor(featureSymbol);
   const featureToggle = useContext(FeatureToggleContext);
 
-  console.log(feature, featureToggle, featureToggle[feature]);
   return Object.prototype.hasOwnProperty.call(featureToggle, feature) && featureToggle[feature] !== null
     ? featureToggle[feature]
     : true;
